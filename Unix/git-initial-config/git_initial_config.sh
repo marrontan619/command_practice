18a3aa793d12d608e3d6aaa7fff3d2d32e460cdc
Ryota Aoki <marrontan619.emobile.ne.jp>
2014-03-08 15:32:40 +0900

    環境作るたびに面倒なgit configの設定をまとめた。
    次期課題：user.name、user.emailを対話的に入力させて設定

---
+++ Unix/git-initial-config/git_initial_config.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+git config --global core.autocrlf input
+git config --global core.editor vim.tiny
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.com commit
