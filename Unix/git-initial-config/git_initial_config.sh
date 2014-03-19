95f6496de2e9954c9821ba8b07a1ac2905105a62
Ryota <marrontan619@gmail.com>
2014-03-20 03:15:58 +0900

    使ってみて足りなかった部分を追加
    ・シェバンミスってた

--- Unix/git-initial-config/git_initial_config.sh
+++ Unix/git-initial-config/git_initial_config.sh
@@ -1,6 +1,6 @@
-#!bin/sh
+#!/bin/sh
 git config --global core.autocrlf input
-git config --global core.editor vim.tiny
+git config --global core.editor vim
 git config --global color.status auto
 git config --global color.branch auto
 git config --global color.diff auto
@@ -8,3 +8,4 @@ git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.com commit
+git config --global gui.encoding utf-8
