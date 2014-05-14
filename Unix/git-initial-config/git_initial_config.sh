4990f0632b4450efa6465138b175fac3ad638a82
Ryota <marrotan619@gmail.com>
2014-05-15 02:33:39 +0900

    ユーザー名とメールアドレスを、入力値で設定するようにした。
    * ついでに使ってみていまいち設定が変わった実感がなかったので、メッセージ出力。
    * 設定内容を出力するのでもいいかもしれない。

--- Unix/git-initial-config/git_initial_config.sh
+++ Unix/git-initial-config/git_initial_config.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+read -p "User Name?: " uname
+git config --global user.name "${uname}"
+read -p "Email?: " email
+git config --global user.email "${email}"
 git config --global core.autocrlf input
 git config --global core.editor vim
 git config --global color.status auto
@@ -10,3 +14,4 @@ git config --global alias.br branch
 git config --global alias.com commit
 git config --global gui.encoding utf-8
 git config --global pull.rebase true
+echo Done!
